596d7250344324e5d0a4a19649e22ae1156380ba
jose davalos <tx_96@hotmail.com>
2020-11-06 17:58:33 -0300

    trabajamos con API

--- public/javascripts/js/map.js
+++ public/javascripts/js/map.js
@@ -7,3 +7,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([-25.507786, -57.364506]).addTo(map);
 L.marker([-25.510417, -57.363395]).addTo(map);
 L.marker([-25.508287, -57.365476]).addTo(map);
+
+$.ajax ({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+
+        
+    }
+})
